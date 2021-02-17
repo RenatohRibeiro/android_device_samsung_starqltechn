386b6afc1b83947b49fead550c557ec5240d2bba
renatohribeiro <renatohribeh@outlook.com>
2021-02-17 22:31:21 +0000

    Pixel Experience bringup

---
+++ aosp_starqltechn.mk
@@ -0,0 +1,6 @@
+$(call inherit-product, device/samsung/starqltechn/full_starqltechn.mk)
+
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+PRODUCT_NAME := aosp_starqltechn
+BOARD_VENDOR := samsung
